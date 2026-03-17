6de1d406dcf6afc8372dfa821a57b110cbc5a6b0
omxmishra <om.mishra.2410@gmail.com>
2026-03-17 16:29:58 +0530

    Add prediction module in src for model inference

--- src/predict.py
+++ src/predict.py
@@ -1,13 +1,18 @@
 import joblib
 import json
 import pandas as pd
+import os
 
-# Load model
-model = joblib.load("models/aviation_disruption_model.pkl")
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Load columns
-with open("models/model_columns.json") as f:
+model_path = os.path.join(BASE_DIR, "models", "aviation_disruption_model.pkl")
+columns_path = os.path.join(BASE_DIR, "models", "model_columns.json")
+
+model = joblib.load(model_path)
+
+with open(columns_path) as f:
     columns = json.load(f)
+
 
 def predict_disruption(input_dict):
     df = pd.DataFrame([input_dict])
